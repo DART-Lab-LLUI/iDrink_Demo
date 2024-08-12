fe69940b9d0e937b7c079568ec753757561c95db
j-ullrich <104524717+j-ullrich@users.noreply.github.com>
2024-08-12 17:55:43 +0200

    Added trial-object creation and video cutting.
    Will add Pose2Sim Pipeline tomorrow.

---
+++ src/iDrink/iDrinkVisualInput.py
@@ -0,0 +1,97 @@
+# Import Libraries
+import os
+import re
+
+import cv2
+import numpy as np
+
+def cut_videos_to_trials(video_files, trial):
+    """
+    Cuts the recorded videos into the individual trials.
+    :param video_files: List of video files
+    :param trial: Trial Object
+    :return:
+
+    Iterates through all the videos and only uses frames of the given trial.
+
+    The video files are then stored in the recordings folder of the trial.
+
+    If clean_video True, the video is checked for frame duplicates.
+    """
+    import queue
+    from tqdm import tqdm
+    from threading import Thread
+
+    def frame_loader_cut(cap, frame_start, frame_end, frame_queue, ret_queue):
+
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_start)
+        frame_count = frame_start
+        while True:
+
+            ret, frame = cap.read()
+            if not ret or frame_count >= frame_end:
+                break
+
+            ret_queue.put(ret)
+            frame_queue.put(frame)
+            frame_count += 1
+
+        ret_queue.put(None)
+        frame_queue.put(None)
+
+
+    video_files_new = []
+    for video in video_files:
+        cap = cv2.VideoCapture(video)
+        if not cap.isOpened():
+            print("Error opening file.")
+            return
+        frame_queue = queue.Queue()
+        ret_queue = queue.Queue()
+
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        match = re.search(r"cam\d+", video)
+        if match:
+            # If a match is found, extract the matched string and add it to the cam_names list
+            cam = match.group()
+        else:
+            cam = "cam_unknown"
+        file_extension = os.path.splitext(video)[1]
+
+        video_new = os.path.realpath(os.path.join(trial.dir_recordings, f'{trial.identifier}_{cam}{file_extension}'))
+
+        # Retrieve start and end frame of the trial
+        frame_start = trial.onset
+        frame_end = trial.offset
+        total_frames_of_trial = int(frame_end - frame_start)
+
+        thread_frame = Thread(target=frame_loader_cut, args=(cap, frame_start, frame_end, frame_queue, ret_queue))
+        thread_frame.start()
+
+        video_files_new.append(video_new)
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        out = cv2.VideoWriter(video_new, fourcc, fps, (width, height))
+
+        pbar = tqdm(total=total_frames_of_trial, desc="Processing Video", unit="frame")
+        # Run and write frames until last frame
+
+        print(f"starting with {trial.identifier}: {os.path.basename(video_new)}")
+        while True:
+            ret = ret_queue.get()
+            frame = frame_queue.get()
+            if not ret:
+                break
+
+            out.write(frame)
+            pbar.update(1)
+
+        pbar.close()
+        thread_frame.join()
+        cap.release()
+        out.release()
+        print(f"Videofile written: {video_new}")
+
+    return video_files_new
---
+++ src/iDrinkHub.py
@@ -0,0 +1,126 @@
+import glob
+import os
+import sys
+import time
+import re
+import shutil
+from tqdm import tqdm
+
+import argparse
+import pandas as pd
+
+from iDrink import iDrinkTrial, iDrinkPoseEstimation, iDrinkVisualInput
+
+from Pose2Sim import Pose2Sim
+
+"""
+This File runs the iDrink Pipeline for the Demo for September 2024.
+
+It gets 
+"""
+dir_root = os.path.realpath(r"C:\iDrink_Demo")
+dir_root_recs = os.path.join(dir_root, r"bids_root_folder")
+dir_mot_out = None
+
+dir_session_data = os.path.join(dir_root, "session_data")
+
+def create_trials(id_s, id_p, df_events, task, calib_file):
+    """
+    Create Trial objects from the events dataframe.
+
+    :param id_s: session_ID
+    :param id_p: participant_ID
+    :param df_events: Dataframe with events - onset, offset, trial_id
+    :return trials: List of iDrinkTrial objects
+    """
+    trials = []
+    n_trials = df_events.shape[0]
+
+    for i in range(n_trials):
+        onset = df_events.loc[i, "onset"]
+        offset = df_events.loc[i, "offset"]
+        trial_id = df_events.loc[i, "trial_id"]
+
+        id_t = f"T{trial_id:03d}"
+        id_s = f"S{id_s}"
+        id_p = f"P{id_p}"
+        identifier = f"{id_s}_{id_p}_{id_t}"
+
+        dir_session = os.path.join(dir_session_data, id_s)
+        dir_calib = os.path.join(dir_session, f"{id_s}_{id_p}_Calibration")
+        dir_participant = os.path.join(dir_session, id_p)
+        dir_trial = os.path.join(dir_participant, id_t)
+
+        calib_file_new = os.path.join(dir_calib, os.path.basename(calib_file))
+
+        trial = iDrinkTrial.Trial(identifier=identifier, id_s=id_s, id_p=id_p, id_t = trial_id,
+                                  onset=onset, offset=offset, assessement="Drinking-Task", task=task,
+                                  dir_trial=dir_trial, dir_participant=dir_participant, dir_session=dir_session,
+                                  dir_root=dir_root,
+                                  path_calib=calib_file_new)
+
+        trial.create_trial()
+        trial.load_configuration()
+
+        trial.config_dict.get("project").update({"project_dir": trial.dir_trial})
+        trial.config_dict['pose']['pose_framework'] = trial.used_framework
+        trial.config_dict['pose']['pose_model'] = trial.pose_model
+
+        trial.save_configuration()
+
+        if not os.path.exists(dir_calib):
+            os.makedirs(dir_calib, exist_ok=True)
+        shutil.copy2(calib_file, dir_calib)
+
+        trials.append(trial)
+
+    return trials
+
+def run(id_s, id_p, task='drink'):
+
+
+    """Get video files, calibration_file and events file"""
+    dir_measurement = os.path.join(dir_root_recs, f"sub-{id_p}", f"ses-{id_s}")
+    dir_mot_out = os.path.join(dir_measurement, "motion")
+
+    calib_file = glob.glob(os.path.join(dir_measurement, "**",  "*calibration.toml"))[0]
+    video_files = glob.glob(os.path.join(dir_measurement, "video", f"*task-{task}*.mp4"))
+    cams = [re.search(r"cam\d", video)[0] for video in video_files]
+
+    df_events = pd.read_csv(glob.glob(os.path.join(dir_measurement, "video", f"*task-{task}_events.csv"))[0])
+
+    """Create trial Objects and Folder Structure for Pipeline"""
+    trials = create_trials(id_s, id_p, df_events, task, calib_file)
+
+
+
+
+    for trial in trials:
+        #Cut recording down to individual trials
+
+        videos = iDrinkVisualInput.cut_videos_to_trials(video_files, trial)
+
+
+        """Run Pipeline for each trial"""
+        trial.config_dict["pose"]["videos"] = videos
+        trial.config_dict["pose"]["cams"] = cams
+
+        # TODO: Add that somewhere before Pose Estimation
+
+
+
+    pass
+
+
+if __name__ == '__main__':
+    # Parse command line arguments
+
+    if sys.gettrace() is not None:
+        print("Debug Mode is activated\n"
+              "Starting debugging script.")
+
+        id_s = "rest"
+        id_p = "4a2"
+        task = "drink"
+
+    run(id_s, id_p, task)
